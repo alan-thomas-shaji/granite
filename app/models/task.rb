4a8691362945f6bda49c01689e63714dd56a6e17
Alan Shaji <alanthomasshaji079@gmail.com>
2022-09-02 12:42:26 +0530

    Added constraint to make body not nullable

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+    MAX_BODY_LENGTH = 125
+    validates :body, presence: true, length: { maximum: 125 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_122226) do
+ActiveRecord::Schema.define(version: 2022_09_02_065936) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
