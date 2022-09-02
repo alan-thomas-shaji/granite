6e069fbd8310e36080e767a8750c4aac299edf5d
Alan Shaji <alanthomasshaji079@gmail.com>
2022-09-02 14:53:21 +0530

    Added git hooks, eslint, prettier, rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
-    MAX_BODY_LENGTH = 125
-    validates :body, presence: true, length: { maximum: 125 }
+  MAX_BODY_LENGTH = 125
+  validates :body, presence: true, length: { maximum: 125 }
 end
