afbcb17e2510ac6ed685de2d19be098f6c063796
Alan Shaji <alanthomasshaji079@gmail.com>
2022-09-06 15:16:36 +0530

    Changed aliases back to original paths

--- app/javascript/src/components/Common/Toastr.jsx
+++ app/javascript/src/components/Common/Toastr.jsx
@@ -1,24 +1,26 @@
-import { TOASTER_OPTIONS } from "constants";
-
 import React from "react";
 
 import { toast } from "react-toastify";
+import { TOASTR_OPTIONS } from "src/constants";
 
-const ToastrComponent = ({ message }) => (
-  <div className="flex flex-row items-start justify-start">
-    <p className="mx-4 font-medium leading-5 text-white">{message}</p>
-  </div>
-);
+// eslint-disable-next-line arrow-body-style
+const ToastrComponent = ({ message }) => {
+  return (
+    <div className="flex flex-row items-start justify-start">
+      <p className="mx-4 font-medium leading-5 text-white">{message}</p>
+    </div>
+  );
+};
 
 const showToastr = message => {
-  toast.success(<ToastrComponent message={message} />, TOASTER_OPTIONS);
+  toast.success(<ToastrComponent message={message} />, TOASTR_OPTIONS);
 };
 
 const isError = e => e && e.stack && e.message;
 
 const showErrorToastr = error => {
   const errorMessage = isError(error) ? error.message : error;
-  toast.error(<ToastrComponent message={errorMessage} />, TOASTER_OPTIONS);
+  toast.error(<ToastrComponent message={errorMessage} />, TOASTR_OPTIONS);
 };
 
 const Toastr = {
