fdde1969b92b4cfdab08041438c4c07c2dda23b3
Alan Shaji <alanthomasshaji079@gmail.com>
2022-09-08 22:40:28 +0530

    Added association in User model for assigned tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   MAX_NAME_LENGTH = 255
+  belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
+  has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
 
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
 end
