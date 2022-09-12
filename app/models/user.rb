5c79680f1c0c29719db551f1ab9fa64cb4da6c5a
Alan Shaji <alanthomasshaji079@gmail.com>
2022-09-12 16:50:11 +0530

    Added comments to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
-  MAX_NAME_LENGTH = 35
-  MAX_EMAIL_LENGTH = 255
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
+  has_many :comments, dependent: :destroy
+
+  before_destroy :assign_tasks_to_task_owners
+
+  MAX_NAME_LENGTH = 255
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  MAX_EMAIL_LENGTH = 255
+
   has_secure_password
   has_secure_token :authentication_token
-  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
+
+  validates :name, { presence: true, length: { maximum: MAX_NAME_LENGTH } }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { maximum: MAX_EMAIL_LENGTH },
     format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }, if: -> { password.present? }
   validates :password_confirmation, presence: true, on: :create
+
   before_save :to_lowercase
-  before_destroy :assign_tasks_to_task_owners
 
   private
 
